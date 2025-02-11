f76dab619a06d6fd267e8e1f747f34bc8b0135ef
YerayRodriguezLopez <yeray.rodriguez.7e5@itb.cat>
2025-02-11 13:48:59 +0100

    Acabado

--- composeApp/src/jvmMain/kotlin/Hotrun.kt
+++ composeApp/src/jvmMain/kotlin/Hotrun.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import java.awt.Dimension
-import cat.itb.m78.exercices.HelloWorldApp
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 fun main() = application {
@@ -15,7 +14,7 @@ fun main() = application {
     ) {
         window.minimumSize = Dimension(350, 600)
         DevelopmentEntryPoint {
-            TrivialApp()
+            trivialApp()
         }
     }
 }
